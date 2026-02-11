f4e5576ff28fbd9523fbf4dee179dc328b394c6d
İlsu Sunal <148697098+ilsusunal@users.noreply.github.com>
2026-02-11 10:54:00 +0300

    fix category card styling

--- src/features/catalog/components/categories/CategoryGrid.tsx
+++ src/features/catalog/components/categories/CategoryGrid.tsx
@@ -10,7 +10,7 @@ interface CategoryGridProps {
 export default function CategoryGrid({ data }: CategoryGridProps) {
   return (
     <>
-      <div className="mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-4 auto-rows-max">
+      <div className="mx-auto grid grid-cols-2 md:grid-cols-5 gap-4 auto-rows-max">
         {data.map((category) => (
           <CategoryCard category={category} key={category.id} />
         ))}
