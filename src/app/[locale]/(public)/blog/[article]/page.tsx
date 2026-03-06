9bfb181e6cfba373966ac8f220c59cc636cc604b
nıdaturkay <nturkaaayyy@gmail.com>
2026-03-06 16:39:07 +0300

    Add Breadcrumb to blog pages

--- src/app/[locale]/(public)/blog/[article]/page.tsx
+++ src/app/[locale]/(public)/blog/[article]/page.tsx
@@ -1,4 +1,5 @@
 
+import { Breadcrumb } from "@/components/common";
 import BlogDetailContentSection from "@/features/blog/section/BlogDetailContent";
 import BlogDetailHeroSection from "@/features/blog/section/BlogDetailHeroSection";
 import PopularSection from "@/features/blog/section/PopularSection";
@@ -9,6 +10,15 @@ export default function BlogDetailPage() {
 
   return (
     <>
+    <div className="max-w-5xl mx-auto pl-4">
+    <Breadcrumb
+  items={[
+    { name: "Ana Sayfa", href: "/" },
+    { name: "Blog", href: "/blog" },
+    { name: blog.title, href: `/blog/${blog.slug}` },
+  ]}
+/>
+</div>
       <BlogDetailHeroSection data={blog} />
       <BlogDetailContentSection data={blog} />
       <PopularSection data={blogListMock.blogs} />
--- src/app/[locale]/(public)/blog/page.tsx
+++ src/app/[locale]/(public)/blog/page.tsx
@@ -1,3 +1,4 @@
+import { Breadcrumb } from "@/components/common";
 import AllBlogSection from "@/features/blog/section/AllBlogSection";
 import HeroSection from "@/features/blog/section/HeroSection";
 import MostReadSection from "@/features/blog/section/MostReadSection";
@@ -7,7 +8,14 @@ import { blogListMock } from "@/mocks/blogs.mock";
 export default function BlogPage() {
   return (
     <div>
-      <p>Ana Sayfa = Blog</p>
+      <div className="max-w-5xl mx-auto pl-4">
+      <Breadcrumb
+  items={[
+    { name: "Ana Sayfa", href: "/" },
+    { name: "Blog", href: "/blog" },
+  ]}
+/>
+</div>
       <HeroSection data={blogListMock.hero} />
       <MostReadSection data={blogListMock.popular}/>
       <AllBlogSection data={blogListMock.blogs} pagination={blogListMock.pagination}/>
