060edf41048abed69eddbeefb66bcbddf46f5b5f
nıdaturkay <nturkaaayyy@gmail.com>
2026-04-03 16:24:49 +0300

    Add streamer application page

---
+++ src/app/[locale]/(private)/user/streamer-application/page.tsx
@@ -0,0 +1,9 @@
+import { UserPageHeader } from "@/features/user/components";
+
+export default function Streamers() {
+    return (
+        <div>
+             <UserPageHeader title="Yayıncı Başvurusu"/>
+        </div>
+    )
+}
